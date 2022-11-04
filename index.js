9516564f323c34a3f66e43cdfd23bbdb1659f565
Mateus Borges <borgesm.work@gmail.com>
2022-11-03 22:29:00 -0300

    feat: Add validation to POST (no blank inputs & no equal usernames)

--- index.js
+++ index.js
@@ -4,39 +4,57 @@ import express from 'express';
 const app = express();
 app.use(cors());
 app.use(express.json());
-let username;
-let avatar;
 const users = [];
 const tweets = [];
 
 app.post('/sign-up', (req, res) => {
-	username = req.body.username;
-	avatar = req.body.avatar;
+	const { username, avatar } = req.body;
+
+	if (!username || !avatar) {
+		res.status(400).send('Todos os campos são obrigatórios!');
+		return;
+	}
+
 	if (
 		!users.find((e) => {
 			if (e.username === username && e.avatar === avatar) {
-				return true;
+				res.status(409).send('Esse usuário já existe');
+				return;
 			}
 		})
 	) {
 		users.push(req.body);
 	}
-	res.send('OK');
+
+	res.status(201).send(users);
 });
 
 app.get('/tweets', (req, res) => {
 	const newTweets = [];
 	let i = tweets.length;
-	while (newTweets.length !== 10) {
-		newTweets.push(tweets[i - 1]);
-		i--;
+	if (tweets.length !== 0) {
+		while (newTweets.length !== 10 && tweets[i - 1] !== undefined) {
+			newTweets.push(tweets[i - 1]);
+			i--;
+		}
 	}
 	res.send(newTweets);
 });
 
 app.post('/tweets', (req, res) => {
-	tweets.push({ username: username, avatar: avatar, tweet: req.body.tweet });
-	res.send(tweets);
+	const { username, tweet } = req.body;
+
+	if (!username || !tweet) {
+		res.status(400).send('Todos os campos são obrigatórios!');
+		return;
+	}
+
+	const avatar = users.filter((e) => e.username === username);
+
+	tweets.push({ username, avatar: avatar[0].avatar, tweet });
+	console.log(tweets);
+
+	res.sendStatus(201);
 });
 
-app.listen(5000);
+app.listen(5000, () => console.log('Running server on http://localhost:5000'));
