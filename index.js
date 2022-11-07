fb1a52a7d83bd64762a5333025b9e976743b8ae0
Mateus Borges <borgesm.work@gmail.com>
2022-11-07 17:52:41 -0300

    feat: load more tweets with query string ?page

--- index.js
+++ index.js
@@ -30,14 +30,33 @@ app.post('/sign-up', (req, res) => {
 });
 
 app.get('/tweets', (req, res) => {
-	const newTweets = [];
-	let i = tweets.length;
-	if (tweets.length !== 0) {
-		while (newTweets.length !== 10 && tweets[i - 1] !== undefined) {
-			newTweets.push(tweets[i - 1]);
-			i--;
+	const page = parseInt(req.query.page);
+	let newTweets = [];
+	let firstIndex = 10 * (page - 1);
+
+	if (!isNaN(page)) {
+		if (tweets.length !== 0) {
+			for (let maxLength = 0; maxLength < 10; maxLength++) {
+				if (tweets[firstIndex] !== undefined) {
+					newTweets.push(tweets[firstIndex]);
+					console.log('-------------', newTweets);
+					firstIndex++;
+				} else {
+					firstIndex++;
+				}
+			}
+		}
+	} else {
+		console.log('else');
+		const i = tweets.length;
+		if (tweets.length !== 0) {
+			while (newTweets.length < 10 && tweets[i - 1] !== undefined) {
+				newTweets.push(tweets[i - 1]);
+				i--;
+			}
 		}
 	}
+	console.log(newTweets);
 	res.send(newTweets);
 });
 
@@ -48,7 +67,8 @@ app.get('/tweets/:USERNAME', (req, res) => {
 });
 
 app.post('/tweets', (req, res) => {
-	const { username, tweet } = req.body;
+	const { tweet } = req.body;
+	const username = req.headers.user;
 
 	if (!username || !tweet) {
 		res.status(400).send('Todos os campos são obrigatórios!');
@@ -56,9 +76,8 @@ app.post('/tweets', (req, res) => {
 	}
 
 	const avatar = users.filter((e) => e.username === username);
-
 	tweets.push({ username, avatar: avatar[0].avatar, tweet });
-
+	console.log(tweets);
 	res.sendStatus(201);
 });
 
